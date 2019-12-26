517ac86f562b78dc0470b9934a45182961220782
Adhithyan t pillai <adhithyantpillaiofficial@gmail.com>
2019-12-26 15:47:17 +0530

    Add files via upload

---
+++ sketch_mar15b.ino
@@ -0,0 +1,330 @@
+
+
+#include <Adafruit_GFX.h>    /
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <SD.h>
+#include <stdint.h>
+#include "TouchScreen.h"
+#define WHITE   0xFFFF
+int i;
+TSPoint p;
+#define  GREEN   0x07E0
+#define YP A1 
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 7   // can be a digital pin
+#define XP 6   // can be a digital pin
+int m;
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+#define SD_CS 10     
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, A4);
+
+void setup()
+{
+  Serial.begin(9600);
+          
+  tft.reset();
+
+ 
+  tft.begin(0x9341);
+
+  if (!SD.begin(SD_CS)) {
+ //  progmemPrintln(PSTR("failed!"));
+    return;
+  }
+ // progmemPrintln(PSTR("OK!"));
+
+ 
+  delay(1000);
+  bmpDraw("welcome.bmp", 0, 0);
+  delay(5000);
+   
+}
+
+void loop()
+{
+
+
+ if( Serial.available()>0){
+  String message = readSerialMessage();
+   if( find(message,"ump")){
+   bmpDraw("hump.bmp", 0, 0);
+   delay(4000);}
+    if( find(message,"raffic")){
+   bmpDraw("traffic.bmp", 0, 0);
+   delay(2000);}
+   //toll
+  if( find(message,"oll")){
+   bmpDraw("toll.bmp", 0, 0);
+   
+  //Serial.print("toll decteted");
+  waitOneTouch();
+ //Serial.println(p.x);
+if(p.x<560)
+{
+  
+Serial.print("radyes");
+bmpDraw("tolly.bmp", 0, 0);
+
+
+
+delay(3000);
+}else{
+    Serial.print("radnop");
+  bmpDraw("tolln.bmp", 0, 0);
+ 
+
+
+
+  delay(3000);
+}
+ 
+      }
+  delay(100);
+
+
+   if( find(message,"sasi")){
+    
+    
+    tft.fillScreen(GREEN); 
+    tft.setCursor(0, 0);
+  tft.setTextColor(WHITE);  tft.setTextSize(5);
+  tft.println("SASI!");
+  for(i=30;i>0;i--)
+  {
+  
+ tft.setTextColor(WHITE);  tft.setTextSize(5);
+   tft.println(i);
+   delay(500);
+    
+    }
+  
+  
+  }
+  
+  }}
+
+
+
+
+  String  readSerialMessage(){
+  char value[100]; 
+  int index_count =0;
+  while(Serial.available()>0){
+    value[index_count]=Serial.read();
+    index_count++;
+    value[index_count] = '\0'; // Null terminate the string
+  }
+  String str(value);
+  str.trim();
+  return str;
+}
+
+boolean find(String string, String value){
+  if(string.indexOf(value)>=0)
+    return true;
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define BUFFPIXEL 20
+
+void bmpDraw(char *filename, int x, int y) {
+
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel in buffer (R+G+B per pixel)
+  uint16_t lcdbuffer[BUFFPIXEL];  // pixel out buffer (16-bit per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+  uint8_t  lcdidx = 0;
+  boolean  first = true;
+
+  if((x >= tft.width()) || (y >= tft.height())) return;
+
+  Serial.println();
+  progmemPrint(PSTR("Loading image '"));
+  Serial.print(filename);
+  Serial.println('\'');
+  // Open requested file on SD card
+  if ((bmpFile = SD.open(filename)) == NULL) {
+    progmemPrintln(PSTR("File not found"));
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    progmemPrint(PSTR("File size: ")); Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    progmemPrint(PSTR("Image Offset: ")); Serial.println(bmpImageoffset, DEC);
+    // Read DIB header
+    progmemPrint(PSTR("Header size: ")); Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      progmemPrint(PSTR("Bit Depth: ")); Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+
+        goodBmp = true; // Supported BMP format -- proceed!
+        progmemPrint(PSTR("Image size: "));
+        Serial.print(bmpWidth);
+        Serial.print('x');
+        Serial.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+        if((x+w-1) >= tft.width())  w = tft.width()  - x;
+        if((y+h-1) >= tft.height()) h = tft.height() - y;
+
+        // Set TFT address window to clipped image bounds
+        tft.setAddrWindow(x, y, x+w-1, y+h-1);
+
+        for (row=0; row<h; row++) { // For each scanline...
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+            pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each column...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              // Push LCD buffer to the display first
+              if(lcdidx > 0) {
+                tft.pushColors(lcdbuffer, lcdidx, first);
+                lcdidx = 0;
+                first  = false;
+              }
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+            lcdbuffer[lcdidx++] = tft.color565(r,g,b);
+          } // end pixel
+        } // end scanline
+        // Write any remaining data to LCD
+        if(lcdidx > 0) {
+          tft.pushColors(lcdbuffer, lcdidx, first);
+        } 
+        progmemPrint(PSTR("Loaded in "));
+        Serial.print(millis() - startTime);
+        Serial.println(" ms");
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) progmemPrintln(PSTR("BMP format not recognized."));
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
+// Copy string from flash to serial port
+// Source string MUST be inside a PSTR() declaration!
+void progmemPrint(const char *str) {
+  char c;
+  while(c = pgm_read_byte(str++)) Serial.print(c);
+}
+
+// Same as above, with trailing newline
+void progmemPrintln(const char *str) {
+  progmemPrint(str);
+  Serial.println();}
+TSPoint waitOneTouch()
+{
+  
+  //TSPoint p;
+  
+  do {
+    p= ts.getPoint(); 
+  
+    pinMode(XM, OUTPUT); //Pins configures again for TFT control
+    pinMode(YP, OUTPUT);
+  
+  } while((p.z < MINPRESSURE )|| (p.z > MAXPRESSURE));
+  
+  return p;
+}
+
+
+  
+
+
+ 
+
